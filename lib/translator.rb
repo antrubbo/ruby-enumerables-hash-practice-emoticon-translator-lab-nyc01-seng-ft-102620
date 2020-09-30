398b66e73cf97e2708d6a684fa1bc87507e1ee1a
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-30 14:27:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,22 @@
 # require modules here
+require 'pry' 
+require 'yaml'
 
-def load_library
-  # code goes here
-end
+def load_library(emoticons)
+  faces = YAML.load_file('lib/emoticons.yml')
+  # binding.pry
+  faces.each_with_object({}) do |(key, value), new_hash|
+    # binding.pry
+    value.each do |e|
+      # binding.pry
+      if !new_hash[e]
+        new_hash[e] = {}
+      end
+      new_hash.update(e.to_s)
+      binding.pry
+    end  
+  end
+end  
 
 def get_japanese_emoticon
   # code goes here
