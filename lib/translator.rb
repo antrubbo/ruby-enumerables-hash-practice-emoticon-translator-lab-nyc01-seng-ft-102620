7f2de378eed841ebb9de56a907456df249a22f2e
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-30 16:08:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ def load_library(file)
 end
 
 def get_japanese_emoticon(file, emoticon)
-  load_library(file)
+  load_library(file).each do |key, value|
+    binding.pry
 end
 
 def get_english_meaning(file, emoticon)
