c6cd8ba78d0a1b743944578490784dc4e4b6f4ba
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-30 14:42:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,10 @@ require 'yaml'
 
 def load_library(emoticons)
   faces = YAML.load_file('lib/emoticons.yml')
-  # binding.pry
   faces.each_with_object({}) do |(key, value), new_hash|
-    # binding.pry
-    value.each do |e, j|
-      # binding.pry
+    value.each do |e|
       if !new_hash[e]
         new_hash[e] = {}
-      end
-      if !new_hash[j]
-        new_hash[j] = {}
       end
       binding.pry
     end  
